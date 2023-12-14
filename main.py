426c4a3f586f6280eeef752910ef564f8214aaa5
chudn <Chudnenkov93@gmail.com>
2023-12-15 01:33:58 +0200

    код ДЗ№1

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 num1 = int(input("Ведіть перше число від 0 до 10:"))
 num2 = int(input("Ведіть друге число від 0 до 10:"))
 num3 = int(input("Ведіть трете число від 0 до 10:"))
-if num1 == num2 == num3:
-    print('eror')
-elif num1 < num2 < num3:
+if (num1 < num2) and (num1 < num3):
     print (f'\n{num1}: min')
-elif num2 < num1 < num3:
+elif (num2 < num1) and (num2 < num3):
     print (f'\n{num2}: min')
-elif num3 < num1 < num2:
+elif (num3 < num1) and (num3 < num2):
     print (f'\n{num3}: min')
-if num1 > num2 < num3:
-    print (f'\n{num1}:s')
-elif num1 > num3 < num2:
-    print (f'\n{num1}:s')
-elif num2 > num1 < num3:
-    print (f'\n{num2}:s')
-elif num2 > num3 < num1:
-    print (f'\n{num2}:s')
-elif num3 > num1 < num2:
-    print (f'\n{num3}:s')
-elif num3 > num2 < num1:
-    print (f'\n{num3}:s')
-elif num1 > num2 > num3:
+if (num1 > num2) and(num1 < num3):
+    print (f'\n{num1}:Середнє арефметичне')
+elif (num1 > num3) and (num1 < num2):
+    print (f'\n{num1}:Середнє арефметичне')
+elif (num2 > num1) and (num2 < num3):
+    print (f'\n{num2}:Середнє арефметичне')
+elif (num2 > num3) and (num2 < num1):
+    print (f'\n{num2}:Середнє арефметичне')
+elif (num3 > num1) and (num3 < num2):
+    print (f'\n{num3}:Середнє арефметичне')
+elif (num3 > num2) and (num3 < num1):
+    print (f'\n{num3}:Середнє арефметичне')
+if (num1 > num2) and (num1 > num3):
     print (f'\n{num1}: max')
-elif num2 > num1 > num3:
+elif (num2 > num1) and (num2 > num3):
     print (f'\n{num2}: max')
-elif num3 > num2 > num1:
+elif (num3 > num2) and (num3 > num1):
     print (f'\n{num3}: max')
+
+print('\n\tend!')
+
