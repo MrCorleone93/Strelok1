de8fa20fc3a22dd35b745b4782fb0140f6e576ef
chudn <Chudnenkov93@gmail.com>
2023-12-18 11:03:21 +0200

    код ДЗ№2 Caramba!

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-num1 = int(input('Ведіть перше число:'))
-num2 = int(input('Ведіть друге число:'))
-print('Ведіть дію:\n1:+\n2:-\n3:*\n4:/\n5:**')
-if
-
+num1 = int(input('Ведіть кількість метрів:'))
+if 1<=num1 and num1<=1000:
+    select = int(input('1:Милі\n2:Ярди\n3:Дюйми\n'))
+    if select==1:
+        print(f'result:{num1*0.0006214}')
+    if select==2:
+        print(f'result:{num1*1.0936133}')
+    if select==3:
+        print(f'result:{num1*39.3700787}')
