eae0057c1db72daf9bcaff86c18e19be3e8c28d1
chudn <Chudnenkov93@gmail.com>
2023-12-18 13:29:45 +0200

    код ДЗ робота коду з помилками!

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('initial commit 1')
+try:
+    num = int(input("Enter a number: "))
+    if 1>=num<=7:
+        if num==1:
+            print(f'Понеділок:{num}день!')
+        if num==2:
+            print(f'Вівторок:{num}день!')
+        if num==3:
+            print(f'Середа:{num}')
+        if num==4:
+            print(f'Четверг:{num}')
+        if num==5:
+            print(f'П,ятниця:{num}')
+        if num==6:
+            print(f'Субота:{num}')
+        if num==7:
+            print(f'Неділя:{num}')
+except TypeError as e:
+    print(f"Не коректний тип данних! Ведіть числове значення від 1 до 7:\n\n\t\t\t{num} ???")
