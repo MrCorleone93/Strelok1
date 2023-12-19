6bc98133e7364cea3673eaf6a65400420c6e0885
chudn <Chudnenkov93@gmail.com>
2023-12-20 01:14:33 +0200

    код ДЗ №3!  Спробувати на while true!

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('initial commit 1')
+try:
+    num1 = int(input('Ведіть перше число: '))
+    num2 = int(input('Ведіть друге число: '))
+    print("Ведіть дію: (Додати: + )\n(Відняти: - )\n(Помножити * )\n(Поділити / )")
+    select = input()
+    if select == '+':
+        print(f'result= {num1 + num2}')
+    elif select == '-':
+        print(f'result= {num1 - num2}')
+    elif select == '*':
+        print(f'result= {num1 * num2}')
+    elif select == '/':
+        print(f'result= {num1 / num2}')
+except ZeroDivisionError as Error:
+    print('Error, На нуль не ділиться!')
+except TypeError as Error:
+    print('Error, Некоректний тип даних! ')
+print('end!!!')
