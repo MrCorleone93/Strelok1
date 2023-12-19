00887a51dcde8b8f0436679d323128a103131317
chudn <Chudnenkov93@gmail.com>
2023-12-19 22:29:46 +0200

    код ДЗ №2!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # try:
 #     num1 = int(input("Enter a number: "))
 #     num2 = int(input("Enter another number: "))
+#     if num1 == num2:
+#         print(f"{num1} Числа рівні між собою! {num2}")
 #     while num1 <= num2:
 #         print(f'{num1}')
 #         num1 += 1
