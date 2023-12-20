598966bace5e7820ce7af5e7a2d3c37c3b148c0a
chudn <Chudnenkov93@gmail.com>
2023-12-20 19:34:53 +0200

    код ДЗ №3! Працює

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 try:
     num1 = int(input('Ведіть перше число: '))
     num2 = int(input('Ведіть друге число: '))
-    print("Ведіть дію: (Додати: + )\n(Відняти: - )\n(Помножити * )\n(Поділити / )")
+    print("Ведіть дію:  \n\n(Додати: + ) або (Відняти: - ) або (Помножити: * ) або (Поділити: / )")
     select = input()
-    if select != '+,-,*,/':
-        print(f' Невірна математична дія ! \n >>>{select}<<< ')
+    if select !='+':
+        if select != '-':
+            if select != '*':
+                if select !='/':
+                    print(f' Некоректна дія! \n{select}')
     if select == '+':
-        print(f'result= {num1 + num2}')
-    elif select == '-':
-        print(f'result= {num1 - num2}')
-    elif select == '*':
-        print(f'result= {num1 * num2}')
-    elif select == '/':
-        print(f'result= {num1 / num2}')
+        print(f'result: = {num1+num2}')
+    if select == '-':
+        print(f'result: = {num1-num2}')
+    if select == '*':
+        print(f'result: = {num1*num2}')
+    if select == '/':
+        print(f'result: = {num1/num2}')
 except ZeroDivisionError as Error:
-    print('Error, На нуль не ділиться!')
+    print('Error, >>>На нуль не ділиться!<<<')
+except TypeError as Error:
+    print('Error, >>>Некоректний тип даних!<<< ')
 except ValueError as Error:
-    print(' Error, Некоректний тип даних !\n Ведіть цифрами !')
+    print('Error, >>>Ведіть цифрами і ніяк інакше!<<< ')
 print('end!!!')
