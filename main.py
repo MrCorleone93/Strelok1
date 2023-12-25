bfa36d8c27f2b9c17ee79ed04778c49d3f4bb7ea
chudn <Chudnenkov93@gmail.com>
2023-12-25 21:06:20 +0200

    код ДЗ №1! Працює

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-text='Lexus1991'
-num1=0
-for i in range(len(text)):
-    if text[i]=='1':
-        num1+=1
-    if text[i]=='2':
-        num1+=1
-    if text[i]=='3':
-        num1+=1
-    if text[i]=='4':
-        num1+=1
-    if text[i]=='5':
-        num1+=1
-    if text[i]=='6':
-        num1+=1
-    if text[i]=='7':
-        num1+=1
-    if text[i]=='8':
-        num1+=1
-    if text[i]=='9':
-        num1+=1
-    if text[i]=='0':
-        num1+=1
-result=len(text)-num1
-print(f'Букв = {result}')
-print(f'Цифр = {num1}')
+try:
+    text=input("Ведіть рядок!")
+    num1=0
+    for i in range(len(text)):
+        if text[i]=='1':
+            num1+=1
+        if text[i]=='2':
+            num1+=1
+        if text[i]=='3':
+            num1+=1
+        if text[i]=='4':
+            num1+=1
+        if text[i]=='5':
+            num1+=1
+        if text[i]=='6':
+            num1+=1
+        if text[i]=='7':
+            num1+=1
+        if text[i]=='8':
+            num1+=1
+        if text[i]=='9':
+            num1+=1
+        if text[i]=='0':
+            num1+=1
+    result=len(text)-num1
+    print(f'Букв = {result}')
+    print(f'Цифр = {num1}')
+except Exception as error:
+    print(error)
 
