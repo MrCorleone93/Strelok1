fb148ca9c3fce0273522959bed3645a9c8555814
chudn <Chudnenkov93@gmail.com>
2023-12-26 17:15:44 +0200

    код ДЗ №2! Пацює!

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-Text1=input("Enter1")
-Text2=input("Enter2")
-num1=0
-for i in (Text1):
-    if i == Text2:
-        num1+=1
-print(f'result: {num1}')
+
+# text1=input("Ведіть рядок!")
+# text2=input("Ведіть символи для пошуку!")
+# num1=0
+# for i in (text1):
+#     for j in (text2):
+#         if [i]==[j]:
+#             num1+=1
+# print(f"В рядку знайдено: {num1} символи!")
+
