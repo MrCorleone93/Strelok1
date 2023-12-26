4703d847f2335d2ed599bb881194b8a66d386f1f
chudn <Chudnenkov93@gmail.com>
2023-12-26 16:21:43 +0200

    код ДЗ №2!

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-Text1=input("Enter")
-Text2=input("Enter")
+Text1=input("Enter1")
+Text2=input("Enter2")
+num1=0
 for i in (Text1):
-    i == text2
+    if i == Text2:
+        num1+=1
+print(f'result: {num1}')
