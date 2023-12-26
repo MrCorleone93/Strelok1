4e0e2bfe581f999900455f6e4e1b1c6a623b0945
chudn <Chudnenkov93@gmail.com>
2023-12-26 17:18:20 +0200

    код ДЗ №1! Пацює!

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-try:
-    text=input("Ведіть рядок!")
-    num1=0
-    for i in range(len(text)):
-        if text[i]=='1':
-            num1+=1
-        if text[i]=='2':
-            num1+=1
-        if text[i]=='3':
-            num1+=1
-        if text[i]=='4':
-            num1+=1
-        if text[i]=='5':
-            num1+=1
-        if text[i]=='6':
-            num1+=1
-        if text[i]=='7':
-            num1+=1
-        if text[i]=='8':
-            num1+=1
-        if text[i]=='9':
-            num1+=1
-        if text[i]=='0':
-            num1+=1
-    result=len(text)-num1
-    print(f'Букв = {result}')
-    print(f'Цифр = {num1}')
-except BaseException as error:
-    print(error)
+# try:
+#     text=input("Ведіть рядок!")
+#     num1=0
+#     for i in range(len(text)):
+#         if text[i]=='1':
+#             num1+=1
+#         if text[i]=='2':
+#             num1+=1
+#         if text[i]=='3':
+#             num1+=1
+#         if text[i]=='4':
+#             num1+=1
+#         if text[i]=='5':
+#             num1+=1
+#         if text[i]=='6':
+#             num1+=1
+#         if text[i]=='7':
+#             num1+=1
+#         if text[i]=='8':
+#             num1+=1
+#         if text[i]=='9':
+#             num1+=1
+#         if text[i]=='0':
+#             num1+=1
+#     result=len(text)-num1
+#     print(f'Букв = {result}')
+#     print(f'Цифр = {num1}')
+# except BaseException as error:
+#     print(error)
 
 ### Тут я не розібрався як виключити зі роботи коду подібні знаки(!!"№;%:?*,) так як код читає ті знаки як Букви!
 
