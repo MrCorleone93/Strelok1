ca1f4b57502f12461207c2f8fbe0a7387bbc0efb
chudn <Chudnenkov93@gmail.com>
2023-12-28 22:54:30 +0200

    код ДЗ ! Пацює!

--- main.py
+++ main.py
@@ -35,3 +35,36 @@
 # text3=input('Ведіть слово яким хочете замінити!')
 # result=text1.replace(text2,text3)
 # print(result)
+
+# Завдання №4
+
+# text='0123456789'
+# print(text[2:3])
+# print(text[8:9])
+# print(text[0:5])
+# print(text[0:8])
+# print(text[1:10:2])
+# print(text[0:10:2])
+# text2=text[::-1]
+# print(text2)
+# print(text2[0:10:2])
+# print(len(text))
+
+#Додаткове завдання!
+
+# text = input("Ведіть текст:")
+# num1 = int()
+# num2 = int()
+# num3 = int()
+# for i in (text):
+#     if i == ',':
+#         num1 += 1
+#     if i == '!':
+#         num2 += 1
+#     if (i == '1' or i == '2' or i == '3' or i == '4' or i == '5'
+#             or i == '6' or i == '7' or i == '8' or i == '9' or i == '0'):
+#         num3 += 1
+# print(f'В данному тексті зайдено: '
+#       f'\nкома: {num1} шт.'
+#       f'\nзнак оклику: {num2} шт. '
+#       f'\nцифр в кількості: {num3} шт.')
