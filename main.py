0bf12de006c75537503fd4a6ebaac45e240231de
chudn <Chudnenkov93@gmail.com>
2024-01-01 15:21:11 +0200

    код ДЗ №1 Працює !

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('initial commit 1')
+text_list = [1,2,3,4,5,6,7,8,9,0-1,-2,-3,-4,-5,-6]
+positiv_numbers_list = []
+negativ_numbers_list = []
+odd_numbers_list = []
+even_numbers_list = []
+for number in text_list:
+    if number>=0 and number<10 and number not in positiv_numbers_list:
+        positiv_numbers_list.append(number)
