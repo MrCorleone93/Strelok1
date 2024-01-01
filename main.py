bb1caacf7f5defb34b57b4b4c38f11c3791c840b
chudn <Chudnenkov93@gmail.com>
2024-01-01 18:48:20 +0200

    код ДЗ №2 Працює !

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-text_list = [1,2,3,4,5,6,7,8,9,0-1,-2,-3,-4,-5,-6]
-positiv_numbers_list = []
-negativ_numbers_list = []
-odd_numbers_list = []
-even_numbers_list = []
-for number in text_list:
-    if number>=0 and number<10 and number not in positiv_numbers_list:
-        positiv_numbers_list.append(number)
+# Завдання №2
+
+# text_list = [1,2,3,4,5,6,7,8,9,0-1,-2,-3,-4,-5,-6]
+# positiv_numbers_list = []
+# negativ_numbers_list = []
+# odd_numbers_list = []
+# even_numbers_list = []
+# for number in text_list:
+#     if number>=0 and number<10 and number not in positiv_numbers_list:
+#         positiv_numbers_list.append(number)
+#     if number<=0 and number>=-10:
+#         negativ_numbers_list.append(number)
+#     if number % 2 == 0:
+#         even_numbers_list.append(number)
+#     if number % 2 == 1:
+#         odd_numbers_list.append(number)
+# print("Список парних чисел = {}\nСписок непарних чисел = {}"
+#       "\nСписок негативних чисел = {}\nСписок позитивних чисел = {}"
+#       .format(even_numbers_list,odd_numbers_list,negativ_numbers_list,positiv_numbers_list))
